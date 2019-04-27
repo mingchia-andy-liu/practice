5cb855dc1b452933fe12062411ad24bf0986e44c
mingchia-andy-liu <mingchia.andy.liu@gmail.com>
2019-04-27 15:56:24 -0700

    🗂 Reorder folder & add template

---
+++ daily/test.py
@@ -0,0 +1,15 @@
+# Template
+import unittest
+
+def Solution():
+  print('Implement me.')
+
+class TestCase(unittest.TestCase):
+  def test_num_1(self):
+    self.assertEqual(1, 1)
+
+def main():
+  unittest.main()
+
+if __name__ == "__main__":
+  main()
